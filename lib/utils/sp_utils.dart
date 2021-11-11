b28bd3feec153e636027166d56347bf647a48d6f
and2long <and2long@icloud.com>
2021-11-11 16:22:25 +0800

    升级依赖库版本，优化SP初始化方法。

--- lib/utils/sp_utils.dart
+++ lib/utils/sp_utils.dart
@@ -4,20 +4,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SPUtils {
   SPUtils._internal();
 
-  static SharedPreferences? _spf;
+  static late SharedPreferences _spf;
 
   static Future<SharedPreferences?> init() async {
-    _spf ??= await SharedPreferences.getInstance();
+    _spf = await SharedPreferences.getInstance();
     return _spf;
   }
 
   /// 语言
   static Future<bool> setLanguageCode(String languageCode) {
-    return _spf!.setString('language_code', languageCode);
+    return _spf.setString('language_code', languageCode);
   }
 
   static String? getLanguageCode() {
-    return _spf!.getString('language_code') ??
+    return _spf.getString('language_code') ??
         S.supportedLocales.first.languageCode;
   }
 }
