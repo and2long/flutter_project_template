75a3b35068403dfd96597f1244a7bd937deff13d
and2long <and2long@icloud.com>
2022-04-14 12:27:32 +0800

    适配到Flutter2.10，升级第三方库版本到最新。

--- lib/components/loading_indicator.dart
+++ lib/components/loading_indicator.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class LoadingIndicator extends StatelessWidget {
   const LoadingIndicator({Key? key}) : super(key: key);
