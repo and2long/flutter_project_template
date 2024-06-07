b45915a0f8e4716c309dd5ae7ce063e3b5740409
Lilong Zhang <and2long@gmail.com>
2024-06-07 13:39:29 +0800

    Migrate to Flutter 3.22.2

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
-// NAME         SIZE  WEIGHT  SPACING
-// headline1    96.0  light   -1.5
-// headline2    60.0  light   -0.5
-// headline3    48.0  regular  0.0
-// headline4    34.0  regular  0.25
-// headline5    24.0  regular  0.0
-// headline6    20.0  medium   0.15
-// subtitle1    16.0  regular  0.15
-// subtitle2    14.0  medium   0.1
-// body1        16.0  regular  0.5   (bodyText1)
-// body2        14.0  regular  0.25  (bodyText2) (default)
-// button       14.0  medium   1.25
-// caption      12.0  regular  0.4
+/// | NAME           | SIZE |  WEIGHT |  SPACING |             |
+/// |----------------|------|---------|----------|-------------|
+/// | displayLarge   | 96.0 | light   | -1.5     |             |
+/// | displayMedium  | 60.0 | light   | -0.5     |             |
+/// | displaySmall   | 48.0 | regular |  0.0     |             |
+/// | headlineMedium | 34.0 | regular |  0.25    |             |
+/// | headlineSmall  | 24.0 | regular |  0.0     |             |
+/// | titleLarge     | 20.0 | medium  |  0.15    |             |
+/// | titleMedium    | 16.0 | regular |  0.15    |             |
+/// | titleSmall     | 14.0 | medium  |  0.1     |             |
+/// | bodyLarge      | 16.0 | regular |  0.5     |             |
+/// | bodyMedium     | 14.0 | regular |  0.25    |             |
+/// | bodySmall      | 12.0 | regular |  0.4     |             |
+/// | labelLarge     | 14.0 | medium  |  1.25    |             |
+/// | labelSmall     | 10.0 | regular |  1.5     |             |
+///
+/// ...where "light" is `FontWeight.w300`, "regular" is `FontWeight.w400` and
+/// "medium" is `FontWeight.w500`.
 
 const themeColor = Colors.blue;
 const dividerColor = Color(0xFFE8E8E8);
@@ -23,7 +28,10 @@ class AppTheme {
 
   static ThemeData lightTheme(BuildContext context) {
     return ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: themeColor),
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: themeColor,
+        dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
+      ),
       splashColor: Colors.transparent,
       dividerTheme: DividerThemeData(color: Colors.grey[300], space: 1),
       brightness: Brightness.light,
