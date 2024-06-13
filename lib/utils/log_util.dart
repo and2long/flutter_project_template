2336f9e7e0d0c7bfa831f7fb8ece47f423fd68ed
Lilong Zhang <and2long@gmail.com>
2024-06-13 23:05:24 +0800

    优化日志打印

--- lib/utils/log_util.dart
+++ lib/utils/log_util.dart
@@ -29,7 +29,12 @@ class Log {
     }
   }
 
-  static void mPrint(LogLevel level, String tag, String content) {
+  static void mPrint(
+    LogLevel level,
+    String tag,
+    String content, {
+    bool showDatetime = false,
+  }) {
     // 终端颜色：https://zhuanlan.zhihu.com/p/634706318
     var start = '\x1b[90m';
     const end = '\x1b[0m';
@@ -53,8 +58,8 @@ class Log {
       case LogLevel.ERROR:
         start = red;
     }
-    final message =
-        '$white${DateTime.now()} : $end$start${level.name}: $tag $content$end';
+    String datetimeStr = showDatetime ? '$white${DateTime.now()} ' : '';
+    final message = '$start$datetimeStr[$tag] [${level.name}] : $content$end';
     log(message);
   }
 }
